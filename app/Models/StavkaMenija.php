e63d8c58abb686219f4e679d3f084d23a57e464f
chana-chh <chanasrb@gmail.com>
2020-02-23 16:07:24 +0100

    meni

--- app/Models/StavkaMenija.php
+++ app/Models/StavkaMenija.php
@@ -7,4 +7,20 @@ use App\Classes\Model;
 class StavkaMenija extends Model
 {
     protected $table = 'stavke_menija';
+
+    public function stavkeZaKategoriju($kat)
+    {
+        $sql = "SELECT * FROM {$this->table()} WHERE kategorija = '{$kat}'";
+        return $this->fetch($sql);
+    }
+
+    public function stavkePoKategorijama()
+    {
+        $kategorije = $this->enumOrSetList('kategorija');
+        $rez = [];
+        foreach ($kategorije as $kat) {
+            $rez[$kat] = $this->stavkeZaKategoriju($kategorije);
+        }
+        return $rez;
+    }
 }
